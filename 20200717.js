356e77f5f2f6d048ba74d171aefc33f8727a3f23
kayo-code <kayo_nagata@bita.jp>
2020-07-17 12:13:13 +0900

    回答２

--- 20200717.js
+++ 20200717.js
@@ -1,1 +1,11 @@
 2020/7/17
+function findMultiples(integer, limit) {
+    //your code here
+    const result = []
+    let x = limit / integer
+    for(let i=1 ; i <= x; i++){
+        let multiples = integer * i
+        result.push( multiples )
+    }
+    return result
+}
