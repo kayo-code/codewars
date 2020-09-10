acbe20cb617deab3c468d8c2a85668cc836738bc
bita-kayo <kayo_nagata@bita.jp>
2020-09-10 14:49:44 +0900

    修正

--- 20200910.js
+++ 20200910.js
@@ -1,6 +1,6 @@
 function evenChars(string) {
   const arrString = string.split('')
-  if(arrString.length === 1) return 'invalid string';
+  if(arrString.length < 2 || 100 < arrString.length) return 'invalid string';
   const arrStringEventNumber = arrString.filter((value,index) => index%2 !== 0);
   return arrStringEventNumber
 }
@@ -9,3 +9,10 @@ evenChars ("aiqbuwbjqwbckjdwbwkqbefhglqhfjbwqejbcadn.bcaw.jbhwefjbwqkvbweevkj.bw
 evenChars ("a")
 evenChars ('0')
 evenChars("HgQzF%)iQnIv$NsSbgIGbjtuAV ^)gbNKundefinedUEytafWEUZIDFqFQzRW*yLnIZIqJ*scbyp%Aj$%OXkBSO$uTYeBlPH%mgsDNhJXX)bs)jHuALv")
+
+
+/*
+問題
+https://www.codewars.com/kata/566044325f8fddc1c000002c/solutions/javascript
+ */
+
